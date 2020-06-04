cda264b7149d78952b4de07bd676ecf6ff66abd2
correiathary <tharycorreia@gmail.com>
2020-06-04 12:30:04 -0300

    ajustes finais

--- front-end/src/components/Acoes.jsx
+++ front-end/src/components/Acoes.jsx
@@ -5,10 +5,15 @@ import '../styles/Acoes.css'
 function Acoes() {
   const webSocket = new WebSocket('ws://localhost:8080');
   const [stockData, setStockData] = useState([])
-  const msg = { stocks: ['IET', 'N', 'ZHT', 'V', 'ELY', 'TZW', 'FIK', 'T', 'ZQ', 'NP', 'MJ', 'KG', 'OY', 'ITN', 'OB', 'ACM', 'QQ', 'X', 'XLC', 'S'], event: 'subscribe' }
+  const [newPrice, setNewPrice] = useState([0])
+  const [key, setKey] = useState(['Stock'])
+
+  const msg = { stocks: ['IET', 'N', 'ZHT', 'V', 'ELY', 'TZW', 'FIK', 'T', 'ZQ', 'NP', 
+  'MJ', 'KG', 'OY', 'ITN', 'OB', 'ACM', 'QQ', 'X', 'XLC', 'S'], event: 'subscribe' }
+  //const msg = { stocks: ['IET', 'OB'], event: 'subscribe' }
 
   useEffect(() => {
-    webSocket.onopen = (event) => {
+    webSocket.onopen = () => {
       webSocket.send(JSON.stringify(msg))
     };
 
@@ -18,6 +23,20 @@ function Acoes() {
     }
   }, [])
 
+  setInterval(() => {
+    if (webSocket.readyState == 1) {
+      webSocket.send(JSON.stringify(msg))
+      webSocket.onmessage = (event) => {
+        var value = event.data !== null || event.data !== undefined ? JSON.parse(event.data) : 0
+        var key = event.data !== null || event.data !== undefined ? JSON.parse(event.data) : 0
+        value = value.stocks !== null || value.stocks !== undefined ? Object.values(value.stocks) : 0
+        key = key.stocks !== null || key.stocks !== undefined ? Object.keys(key.stocks) : 0
+        setNewPrice(value)
+        setKey(key)
+      }
+    }
+  }, 3000);
+
   webSocket.onerror = (error) => {
     console.log('WebSocket Error: ' + error);
   };
@@ -26,14 +45,25 @@ function Acoes() {
     console.log('Disconnected from WebSocket.', event);
   };
 
+  function mask(value) {
+    return '$ ' + value.toFixed(3)
+  }
+
   return (
-    <div className='container d-flex justify-content-center row acoes'>
-      {stockData && stockData.map((stock, index) => {
-        return (
-          <Acao acao={stock} key={index} />
-        )
-      })}
+    <div>
+      <div className='container card'>
+        <h1 className='d-flex justify-content-center'> {key[0]}  {mask(newPrice[0])}</h1>
+      </div>
+      <div className='container d-flex justify-content-center row acoes'>
+
+        {stockData && stockData.map((stock, index) => {
+          return (
+            <Acao acao={stock} key={index} />
+          )
+        })}
+      </div>
     </div>
+
   )
 }
 
