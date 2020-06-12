743da86a00071863c8409f7b6973201c83ea3f36
correiathary <tharycorreia@gmail.com>
2020-06-12 14:19:57 -0300

    cores valores

--- front-end/src/components/Acao.jsx
+++ front-end/src/components/Acao.jsx
@@ -4,14 +4,19 @@ import '../styles/Acao.css'
 function Acao(props) {
   let symbol = props.acao.key
   let price = props.acao.value
+  let oldValue = props.acao.oldValue
 
   function mask(value) {
     return '$ ' + value.toFixed(2)
   }
+
   return (
     <div className="col-md-2 card m-2">
       <h2 className='title-card mt-2 m-1 d-flex justify-content-center'> {symbol}</h2>
-      <h3 className='text-center pt-2 text-danger'>{mask(price)}</h3>
+      {(price.toFixed(2) > oldValue.toFixed(2)) ? 
+        <h3 className='text-center pt-2 text-success'>{mask(price)}</h3> : 
+        <h3 className='text-center pt-2 text-danger'>{mask(price)}</h3>
+      }
     </div>
   )
 }
--- front-end/src/components/Acoes.jsx
+++ front-end/src/components/Acoes.jsx
@@ -66,7 +66,7 @@ function Acoes() {
     { key: 'QQ', value: qqValue, oldValue: qqOldValue },
     { key: 'X', value: xValue, oldValue: xOldValue },
     { key: 'XLC', value: xlcValue, oldValue: xlcOldValue },
-    { key: 'S', value: sValue, oldValue: sOldValue }  
+    { key: 'S', value: sValue, oldValue: sOldValue }
   ]
 
   function createWS() {
@@ -101,69 +101,151 @@ function Acoes() {
 
       switch (key[0]) {
         case 'IET':
+          arrEit[0] = arrEit[1]
+          arrEit[1] = newValue
+          setIetOld(Number(arrEit[0]))
           setIet(Number(newValue))
           break
         case 'N':
+          arrN[0] = arrN[1]
+          arrN[1] = newValue
+          setNOld(Number(arrN[0]))
           setN(Number(newValue))
           break
         case 'ZHT':
+          arrZht[0] = arrZht[1]
+          arrZht[1] = newValue
+          setZhtOld(Number(arrZht[0]))
           setZht(Number(newValue))
           break
         case 'V':
+          arrV[0] = arrV[1]
+          arrV[1] = newValue
+          setVOld(Number(arrV[0]))
           setV(Number(newValue))
           break
         case 'ELY':
+          arrEly[0] = arrEly[1]
+          arrEly[1] = newValue
+          setElyOld(Number(arrEly[0]))
           setEly(Number(newValue))
           break
         case 'TZW':
+          arrTzw[0] = arrTzw[1]
+          arrTzw[1] = newValue
+          setTzwOld(Number(arrTzw[0]))
           setTzw(Number(newValue))
           break
         case 'FIK':
+          arrFik[0] = arrFik[1]
+          arrFik[1] = newValue
+          setFikOld(Number(arrFik[0]))
           setFik(Number(newValue))
           break
         case 'T':
+          arrT[0] = arrT[1]
+          arrT[1] = newValue
+          setTOld(Number(arrT[0]))
           setT(Number(newValue))
           break
         case 'ZQ':
+          arrZq[0] = arrZq[1]
+          arrZq[1] = newValue
+          setZqOld(Number(arrZq[0]))
           setZq(Number(newValue))
           break
         case 'NP':
+          arrNp[0] = arrNp[1]
+          arrNp[1] = newValue
+          setNpOld(Number(arrNp[0]))
           setNp(Number(newValue))
           break
         case 'MJ':
+          arrMj[0] = arrMj[1]
+          arrMj[1] = newValue
+          setMjOld(Number(arrMj[0]))
           setMj(Number(newValue))
           break
         case 'KG':
+          arrKg[0] = arrKg[1]
+          arrKg[1] = newValue
+          setKgOld(Number(arrKg[0]))
           setKg(Number(newValue))
           break
         case 'OY':
+          arrOy[0] = arrOy[1]
+          arrOy[1] = newValue
+          setOyOld(Number(arrOy[0]))
           setOy(Number(newValue))
           break
         case 'ITN':
+          arrInt[0] = arrInt[1]
+          arrInt[1] = newValue
+          setIntOld(Number(arrInt[0]))
           setInt(Number(newValue))
           break
         case 'OB':
+          arrOb[0] = arrOb[1]
+          arrOb[1] = newValue
+          setObOld(Number(arrOb[0]))
           setOb(Number(newValue))
           break
         case 'ACM':
+          arrAcm[0] = arrAcm[1]
+          arrAcm[1] = newValue
+          setAcmOld(Number(arrAcm[0]))
           setAcm(Number(newValue))
           break
         case 'QQ':
+          arrQq[0] = arrQq[1]
+          arrQq[1] = newValue
+          setQqOld(Number(arrQq[0]))
           setQq(Number(newValue))
           break
         case 'X':
+          arrX[0] = arrX[1]
+          arrX[1] = newValue
+          setXOld(Number(arrX[0]))
           setX(Number(newValue))
           break
         case 'XLC':
+          arrXlc[0] = arrXlc[1]
+          arrXlc[1] = newValue
+          setXlcOld(Number(arrXlc[0]))
           setXlc(Number(newValue))
           break
         case 'S':
+          arrS[0] = arrS[1]
+          arrS[1] = newValue
+          setSOld(Number(arrS[0]))
           setS(Number(newValue))
           break
         default:
       }
     }
   }
+
+  var arrEit = [0, 0]
+  var arrN = [0, 0]
+  var arrZht = [0, 0]
+  var arrV = [0, 0]
+  var arrEly = [0, 0]
+  var arrTzw = [0, 0]
+  var arrFik = [0, 0]
+  var arrT = [0, 0]
+  var arrZq = [0, 0]
+  var arrNp = [0, 0]
+  var arrMj = [0, 0]
+  var arrKg = [0, 0]
+  var arrOy = [0, 0]
+  var arrInt = [0, 0]
+  var arrOb = [0, 0]
+  var arrAcm = [0, 0]
+  var arrQq = [0, 0]
+  var arrX = [0, 0]
+  var arrXlc = [0, 0]
+  var arrS = [0, 0]
+
 
   useEffect(() => {
     createWS()
